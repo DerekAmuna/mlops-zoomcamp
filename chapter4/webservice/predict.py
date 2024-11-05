a61fbab08dde6cd33f55136871bb23ed875e174c
Derek Amuna <dadadee02@outlook.com>
2024-11-05 12:51:06 +0000

    chapter4 homework

--- chapter4/webservice/predict.py
+++ chapter4/webservice/predict.py
@@ -1,13 +1,13 @@
+# service without mlflow server
 from flask import Flask, request, jsonify
 import pickle
 import mlflow
 from mlflow.tracking import MlflowClient
 
 
-
 MLFLOW_TRACKING_URI = "http://127.0.0.1:5000"
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment('duration-prediction')
+mlflow.set_experiment("duration-prediction")
 client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
 
 # Get the latest run ID
@@ -15,7 +15,7 @@ experiment = mlflow.get_experiment_by_name("duration-prediction")
 runs = mlflow.search_runs(
     experiment_ids=[experiment.experiment_id],
     order_by=["start_time DESC"],
-    max_results=1
+    max_results=1,
 )
 
 if len(runs) == 0:
@@ -25,7 +25,7 @@ RUN_ID = runs.iloc[0].run_id
 print(f"Using Run ID: {RUN_ID}")
 
 # Load model as a PyFuncModel.
-logged_model = f'runs:/{RUN_ID}/model'
+logged_model = f"runs:/{RUN_ID}/model"
 model = mlflow.pyfunc.load_model(logged_model)
 
 dv = client.download_artifacts(run_id=RUN_ID, path="dv.bin")
@@ -35,29 +35,29 @@ with open(dv, "rb") as f_in:
 # with open("intro_lm.bin", "rb") as f_in:
 #     dv, model = pickle.load(f_in)
 
+
 def predict(features):
     X = dv.transform(features)
     y_pred = model.predict(X)
     return y_pred[0]
 
 
-# from here is the flask app code 
+# from here is the flask app code
 app = Flask("duration-prediction")
+
 
 @app.route("/predict", methods=["POST"])
 def predict_endpoint():
-    #input features for ride
+    # input features for ride
     ride = request.get_json()
-    #predict the duration
+    # predict the duration
     y_pred = predict(ride)
-    #return the result
-    result = {
-        "duration": y_pred,
-        'RUN_ID':RUN_ID
-    }
-    return jsonify(result) # jsonify is used to convert the result to a json object
+    # return the result
+    result = {"duration": y_pred, "RUN_ID": RUN_ID}
+    return jsonify(result)  # jsonify is used to convert the result to a json object
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=9696)
 
-#can be quite messy see cleaner version using predict2.py
+# can be quite messy see cleaner version using predict2.py
--- chapter4/webservice/predict3.py
+++ chapter4/webservice/predict3.py
@@ -1,19 +1,19 @@
+# batch deployment
 from flask import Flask, request, jsonify
 import mlflow
 import numpy as np
 
 
-
 MLFLOW_TRACKING_URI = "http://127.0.0.1:5000"
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment('duration-prediction')
+mlflow.set_experiment("duration-prediction")
 
 # Get the latest run ID
 experiment = mlflow.get_experiment_by_name("duration-prediction")
 runs = mlflow.search_runs(
     experiment_ids=[experiment.experiment_id],
     order_by=["start_time DESC"],
-    max_results=1
+    max_results=1,
 )
 
 if len(runs) == 0:
@@ -24,8 +24,8 @@ print(f"Using Run ID: {RUN_ID}")
 
 # Load model as a PyFuncModel.
 # model_run_path = /Users/dna/mlops-zoomcamp/chapter4/webservice/mlflow_artifacts/1/3fb7008b16534ce4b61ea373d96ddf42/artifacts/model
-# can use if mlflow server is down 
-logged_model = f'runs:/{RUN_ID}/model'
+# can use if mlflow server is down
+logged_model = f"runs:/{RUN_ID}/model"
 model = mlflow.pyfunc.load_model(logged_model)
 
 
@@ -38,23 +38,21 @@ def predict(features):
     return y_pred[0]
 
 
-# from here is the flask app code 
+# from here is the flask app code
 app = Flask("duration-prediction")
+
 
 @app.route("/predict", methods=["POST"])
 def predict_endpoint():
-    #input features for ride
+    # input features for ride
     ride = request.get_json()
     print(ride)
-    #predict the duration
+    # predict the duration
     y_pred = predict(ride)
-    #return the result
-    result = {
-        "duration": y_pred,
-        'RUN_ID':RUN_ID
-    }
-    return jsonify(result) # jsonify is used to convert the result to a json object
+    # return the result
+    result = {"duration": y_pred, "RUN_ID": RUN_ID}
+    return jsonify(result)  # jsonify is used to convert the result to a json object
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=9696)
-
