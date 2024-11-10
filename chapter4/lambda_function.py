ff36cc114ff1a1cfc5b14f61df0ac76b58d5bbfe
Derek Amuna <derek@reallygreattech.com>
2024-11-10 18:21:01 +0000

    optional video

---
+++ chapter4/lambda_function.py
@@ -0,0 +1,56 @@
+import json
+import base64
+import boto3
+import os
+import mlflow
+import numpy as np
+
+
+RUN_ID = "e308ab2a149249a4b161cb428b4abc23"
+
+logged_model = f"s3://mlops-derek/1/{RUN_ID}/artifacts/model"
+model = mlflow.pyfunc.load_model(logged_model)
+
+
+PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'ride_preds')
+kinesis_client = boto3.client('kinesis', region_name=os.getenv('AWS_DEFAULT_REGION', 'eu-north-1'),
+                             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+def predict(features):
+    return model.predict(features)
+
+predictions_event = []
+
+def lambda_handler(event, context):
+    for rec in event['Records']:
+        encoded_data = rec['kinesis']['data']
+        decoded_data = base64.b64decode(encoded_data).decode("utf-8")
+        ride_event = json.loads(decoded_data)
+        # print(f'Decode in JSON format:\n\n\n{ride_event}\n\n\n')
+
+        ride = ride_event['ride']
+        ride_id = ride_event['ride_id']
+        prediction = predict(ride)
+        prediction = prediction.tolist() if isinstance(prediction, np.ndarray) else prediction
+        prediction_event  =  {
+            "model":"ride_duration_prediction_model",
+            "version":RUN_ID,
+            "prediction": {
+                "ride_duration":prediction,
+                "ride_id":ride_id
+            }
+        }
+    
+    
+    kinesis_client.put_record(
+            StreamName=PREDICTIONS_STREAM_NAME,
+            Data=json.dumps(prediction_event),
+            PartitionKey=str(ride_id)
+    )
+
+    predictions_event.append(prediction_event)
+
+    return {
+        "predictions":predictions_event
+    }
---
+++ chapter4/test.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+import lambda_function
+
+
+
+event = {
+    "Records": [
+        {
+            "kinesis": {
+                "kinesisSchemaVersion": "1.0",
+                "partitionKey": "1",
+                "sequenceNumber": "49657540666418930171109502259426480070896488208763191298",
+                "data": "eyJyaWRlIjp7IlBVTG9jYXRpb25JRCI6IDEzMCwgIkRPTG9jYXRpb25JRCI6IDIwNSwgInRyaXBfZGlzdGFuY2UiOiAzLjY2fSwgInJpZGVfaWQiOiA0NTkzMDIwMDJ9",
+                "approximateArrivalTimestamp": 1731198238.106
+            },
+            "eventSource": "aws:kinesis",
+            "eventVersion": "1.0",
+            "eventID": "shardId-000000000000:49657540666418930171109502259426480070896488208763191298",
+            "eventName": "aws:kinesis:record",
+            "invokeIdentityArn": "arn:aws:iam::361769579073:role/lambda-kinesis-role",
+            "awsRegion": "eu-north-1",
+            "eventSourceARN": "arn:aws:kinesis:eu-north-1:361769579073:stream/mlops-zoomcamp"
+        }
+    ]
+}
+
+result = lambda_function.lambda_handler(event, None)
+print(result)
